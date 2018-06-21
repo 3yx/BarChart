4a4a2121fbac627c9fab4b7b6ffc5d275ae3ddf2
3yx <deniszukh@gmail.com>
2018-06-21 11:16:42 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function(){
+	
+	// Вычисляем ширину каждого столбца
+	calcWidth = function(){
+		return 100/a.length + '%';
+	}
+
+	// при вводе строки с клавиатуры, удаляем старый график и строку переводим в массив
+	enterMassive = function(){
+		str = prompt('Please, enter your numbers using ",":');
+		a = str.split(',');
+		document.getElementById('graph').innerHTML = '';
+		document.getElementById('value').innerHTML = '';
+		parseMassive();
+	}
+
+	// делаем цветовую градацию
+	whichColor = function(i){
+		return (a[i] <= 5) ? 'green' :
+			(a[i] >= 6 && a[i] <= 10) ? 'yellow' :
+			'red';
+	}
+
+	// создаем график
+	createBar = function(i){
+		var bar = document.createElement('div');
+			bar.id = 'bar';
+			bar.style.height = (a[i] *10) + 'px';
+			bar.style.width = calcWidth();
+			bar.style.backgroundColor = whichColor(i);
+			document.getElementById('graph').appendChild(bar);
+
+		var value = document.createElement('div');
+			value.className = 'value';			
+			value.style.width = calcWidth();
+			value.innerHTML = a[i];
+			document.getElementById('value').appendChild(value);
+	}
+
+	// разбираем массив чисел
+	parseMassive = function(){
+		for (var i = 0; i <= a.length-1; i++) {
+			createBar(i);
+		}
+	};
+
+	// создаем блок для вывода графика
+	createGraph = function(){
+		var graph = document.createElement('div');
+		graph.id = 'graph';
+		document.body.insertBefore(graph, document.body.children[1]);
+
+		var value = document.createElement('div');
+		value.id = 'value';
+		document.body.insertBefore(value, document.body.children[2]);
+	}
+
+	createGraph();
+
+	var a = [5,8,2,1,15,2,3,5,9,11,10,4,3,14,1,7,10,3,2,13];
+
+	parseMassive();
+});
